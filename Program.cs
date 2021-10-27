3069476f954a1a4dd11cd727d9327bb8dcd12377
305-3 <305-3@305-3>
2021-10-27 16:04:53 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace KOLLvar3
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        { string a ;
+            StreamReader sr = new StreamReader("D:/С#/21-22/KOLLvar3/bin/Debug/net5.0/koil.txt");
+            a = sr.ReadToEnd();
+            if(a == string.Empty)
+            {
+                Console.WriteLine("Файл пуст, заполните!");
+            }
+            else
+            Console.WriteLine(a);
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in a)
+            {
+                    if (c == '#')
+                    {
+                    if (stack.Count > 0)
+                        stack.Pop();
+                    }
+                     else
+                    {
+                    stack.Push(c);
+                    }
+            }
+            var array = stack.ToArray();
+            Array.Reverse(array);
+            string s = new string(array);
+                Console.WriteLine(s);
+        }
+    }
+}
